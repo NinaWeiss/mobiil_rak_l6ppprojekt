fef317c79e4629a224ae61c07e3bc8c8de52589f
Mihkel <joll.mihkeel@gmail.com>
2021-05-03 21:15:17 +0300

    Renamed app

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kiddify',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
